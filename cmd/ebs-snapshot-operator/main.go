101eb4e8d301c48a132798e9931d994d774affc2
cricci82 <chris.ricci@coreos.com>
2018-07-31 08:32:40 -0400

    Fixing unescaped json issue

--- cmd/ebs-snapshot-operator/main.go
+++ cmd/ebs-snapshot-operator/main.go
@@ -1,52 +1,55 @@
+package main
+
 import (
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type ec2InstanceMetaData struct {
-	privateIp			string `json:"privateIp"`
-	instanceId			string `json:"instanceId"`
-	billingProducts		string `json:"billingProducts"`
-	instanceType		string `json:"instanceType"`
-	accountId			int    `json:"accountId"`
-	pendingTime			string `json:"pendingTime"`
-	imageId				string `json:"imageId"`
-	kernelId			string `json:"kernelId"`
-	ramdiskId			string `json:"privateIp"`
-	architecture		string `json:"architecture"`
-	region				string `json:"region"`
-	version				string `json:"version"`
-	availabilityZone	string `json:"availabilityZone"`
-	devpayProductCodes	string `json:"devpayProductCodes"`
+	PrivateIP          string `json:"privateIp"`
+	InstanceID         string `json:"instanceId"`
+	BillingProducts    string `json:"billingProducts"`
+	InstanceType       string `json:"instanceType"`
+	AccountID          int    `json:"accountId"`
+	PendingTime        string `json:"pendingTime"`
+	ImageID            string `json:"imageId"`
+	KernelID           string `json:"kernelId"`
+	RamdiskID          string `json:"ramdiskId"`
+	Architecture       string `json:"architecture"`
+	Region             string `json:"region"`
+	Version            string `json:"version"`
+	AvailabilityZone   string `json:"availabilityZone"`
+	DevpayProductCodes string `json:"devpayProductCodes"`
 }
 
-// Get Region of Current Instance
-url := "http://169.254.169.254/latest/dynamic/instance-identity/document"
+func main() {
+	// Get Region of Current Instance
+	url := "http://169.254.169.254/latest/dynamic/instance-identity/document"
+	// url := "http://localhost:8080/metadata"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
 
-req, err := http.NewRequest("GET", url, nil)
-if err != nil {
-	log.Fatal("NewRequest: ", err)
-	return
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+	defer resp.Body.Close()
+	// body, err := ioutil.ReadAll(resp.Body)
+	var metadata ec2InstanceMetaData
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&metadata); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Current Region = ", metadata.Region)
+
+	//sess, err := session.NewSession(&aws.Config{Region: aws.String(metadata.region)})
 }
-
-client := &http.Client{}
-resp, err := client.Do(req)
-if err != nil {
-	log.Fatal("Do: ", err)
-	return
-}
-defer resp.Body.Close()
-var metadata ec2InstanceMetaData
-
-// Use json.Decode for reading streams of JSON data
-if err := json.NewDecoder(resp.Body).Decode(&metadata); err != nil {
-	log.Println(err)
-}
-
-fmt.Println("Current Region = ", metadata.region)
-
-sess, err := session.NewSession(&aws.Config{Region: aws.String(metadata.region)})
